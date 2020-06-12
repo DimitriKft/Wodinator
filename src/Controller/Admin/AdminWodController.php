ff03a0412724d78c5f9086d3971b023fa04f1a31
klopfstein <dimitri.kft@gmail.com>
2020-06-12 10:24:22 +0200

    v1 form is OK yeaaa :D

--- src/Controller/Admin/AdminWodController.php
+++ src/Controller/Admin/AdminWodController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Wod;
+use App\Form\WodType;
 use App\Repository\WodRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminWodController extends AbstractController
 {
@@ -14,8 +18,37 @@ class AdminWodController extends AbstractController
     public function index(WodRepository $repo)
     {
         $wods = $repo->findAll();
-        return $this->render('admin_wod/index.html.twig', [
+        return $this->render('admin_wod/adminWod.html.twig', [
             "wods" => $wods
+        ]);
+    }
+
+    /**
+    * @Route("/admin/wod/create", name="admin_wod_create")
+    * @Route("/admin/wod/{id}", name="admin_wod_update", methods="GET|POST")
+    */
+    public function addAndUpdate(Wod $wod = null,Request $request, EntityManagerInterface $em)
+    {
+        if(!$wod)
+        {
+            $wod = new Wod();
+        }
+
+        $form = $this->createForm(WodType::class, $wod);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $update = $wod->getId() !== null;
+            $em->persist($wod);
+            $em->flush();
+            $this->addFlash("succes", ($update) ? "La modification a été effectuée" : "L'ajout a été effectué");
+            return $this->redirectToRoute('admin_wod');
+        }
+
+        return $this->render('admin_wod/addAndUpdate.html.twig', [
+            "wod" => $wod,
+            "form" => $form->createView(),
+            "isUpdate" => $wod->getId() !== null
         ]);
     }
 }
